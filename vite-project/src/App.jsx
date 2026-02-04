32158562cdc6d60c424660fbede85925c8b8b3f6
mayank <prajapatimayank28020@gmail.com>
2026-02-04 16:28:08 +0530

    land page done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Land_Page from './components/Land_Page'
+import hero from './components/hero'
+
+function App() {
+
+  return (
+    <>
+    <Land_Page />
+    <hero />
+      
+    </>
+  )
+}
+
+export default App
